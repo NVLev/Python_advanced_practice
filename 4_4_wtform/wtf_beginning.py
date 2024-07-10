5f30649a01d88c444cfd8f89a6216d83dcc58d97
NVLev <natalyavladislavna3@gmail.com>
2024-07-10 16:50:48 +0300

    wtf_beginning

--- 4_4_wtform/wtf_beginning.py
+++ 4_4_wtform/wtf_beginning.py
@@ -12,8 +12,20 @@ class RegistrationForm(FlaskForm):
     address = StringField()
 
 
-@app.route("/registration", methods=["POST"])
-def registration():
+@app.route("/register", methods=["POST"])
+def register():
     form = RegistrationForm()
-    if not phone:
-        pass
+
+    if not form.name.data or not form.phone.data or not form.address.data:
+        return f"Invalid input, one field is not filled", 400
+
+    if form.validate_on_submit():
+        name, phone = form.name.data, form.phone.data
+        return f"Successfully registered user {name} with phone +7{phone}"
+
+    return f"Invalid input, {form.errors}", 400
+
+
+if __name__ == "__main__":
+    app.config["WTF_CSRF_ENABLED"] = False
+    app.run(debug=True)
